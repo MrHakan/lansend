2099db588dc04427d8fabd8cd49cec761ccdec29
MrHakan <hakan.efe.gulacti@gmail.com>
2026-09-21 12:53:21 +0100

    Switch to direct SMB constants

--- LANSEND/AppConstants.cs
+++ LANSEND/AppConstants.cs
@@ -4,17 +4,12 @@ namespace LANSEND;
 
 internal static class AppConstants
 {
-    public const string Protocol = "LANSEND/1";
-    public const int DiscoveryPort = 43821;
-    public const int TransferPort = 43822;
     public const string MutexName = "LANSEND_SINGLE_INSTANCE_V1";
     public const string ControlPipeName = "LANSEND_CONTROL_V1";
-    public const int DiscoveryIntervalMilliseconds = 2500;
-    public const int PeerTimeoutSeconds = 9;
-    public const int MaxProtocolLineBytes = 1024 * 1024;
-    public const int MaxFileCount = 10_000;
-    public const long MaxFileSize = 50L * 1024 * 1024 * 1024;
-    public const long MaxTransferSize = 200L * 1024 * 1024 * 1024;
+    public const string DefaultShareName = "LANSEND";
+    public const int SmbPort = 445;
+    public const int SmbProbeTimeoutMilliseconds = 350;
+    public const int MaxScanHosts = 512;
 
     public static readonly JsonSerializerOptions JsonOptions = new()
     {
